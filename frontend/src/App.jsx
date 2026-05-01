a87cf2a83b14634b08843ac7227b04033cee3b96
ANKIT PURI <ankitpuri829@gmail.com>
2026-05-01 22:16:01 +0530

    Corrected API paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         {/* The Login page is the default screen */}
         <Route path="/" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
         
         {/* The Dashboard is accessible at /dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
